8f66807fa6d4a1bba41b9ae8bdb3353338331c19
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2025-03-09 18:24:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,22 +2,24 @@
 #include "eval.h"
 
 int main() {
-  const char* input = "(+ 2 3)";
-  // const char* input = "3";
-  // const char* input = "+";
+  // const char* input = "(+ 2 3)"; // not working
+  // const char* input = "+"; // not working
+  // const char* input = "*"; // working!
+  const char* input = "4";    // working!
 
   Token* tokens = tokenize(input);
 
+  printf("TOKENIZING:\n");
   for (int i = 0; tokens[i].type != TOKEN_EOF; i++) {
     printf("Token Type %d: %s\n", tokens[i].type, tokens[i].value);
   }
 
+  printf("\n\nPARSING:\n");
   SchemeObject* expr = parser(&tokens);
-
   print_scheme_object(expr); printf("\n");
 
+  printf("\n\nEVALUATING:\n");
   SchemeObject* result = eval(expr, init_environment());
-
   print_scheme_object(result);
 
   printf("\n\nFinished executing.");
