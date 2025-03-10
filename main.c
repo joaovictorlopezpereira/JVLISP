f96144443f152179db9fbe266360702e222566c8
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2025-03-10 02:09:00 -0300

    Added TODOS

--- main.c
+++ main.c
@@ -2,19 +2,11 @@
 #include "eval.h"
 
 // TODO: free tokens after parsing
+// TODO: free environment
+// TODO: free Scheme expressions
+// TODO: remove exits and replace with NULL pointers
 
 int main() {
-  // const char* input = "(define x 10)";   // working
-  // const char* input = "(/ 5 2 2)";       // working
-  // const char* input = "(* 1 3 7)";       // working
-  // const char* input = "(+ 5 (- 4 3) 1)"; // working
-  // const char* input = "(- 5 2 1)";       // working
-  // const char* input = "(+ 2 3 7)";       // working
-  // const char* input = "+";               // working
-  // const char* input = "-";               // working
-  // const char* input = "*";               // working
-  // const char* input = "4";               // working
-
   char* input = malloc(256 * sizeof(char));
   Environment* global_environment = init_environment();
 
@@ -24,9 +16,6 @@ int main() {
     SchemeObject* expr = parser(&tokens);                   // READ
     SchemeObject* result = eval(expr, &global_environment); // EVAL
     print_scheme_object(result);                            // PRINT
-    // free_tokens(tokens);
-    free_object(expr);
-    free_object(result);
   }                                                         // LOOP
 
   return 0;
