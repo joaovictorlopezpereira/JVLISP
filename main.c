e60cb572ae1259aace1ebc056c74374f2fa01ae5
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2025-03-09 23:36:33 -0300

    Fixed defined variables from not being added to the environment

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include "parser.h"
 #include "eval.h"
 
+// TODO: free tokens after parsing
+
 int main() {
-  // const char* input = "(define x 10)"; // not working
-  // // const char* input = "(/ 5 2 2)"; // working
-  // // const char* input = "(* 1 3 7)"; // working
-  // // const char* input = "(+ 5 (- 4 3) 1)"; //working
-  // // const char* input = "(- 5 2 1)"; // working
-  // // const char* input = "(+ 2 3 7)"; // working
-  // // const char* input = "+"; // working
-  // // const char* input = "-"; // working
-  // // const char* input = "*"; // working!
-  // // const char* input = "4";    // working!
+  // const char* input = "(define x 10)";   // working
+  // const char* input = "(/ 5 2 2)";       // working
+  // const char* input = "(* 1 3 7)";       // working
+  // const char* input = "(+ 5 (- 4 3) 1)"; // working
+  // const char* input = "(- 5 2 1)";       // working
+  // const char* input = "(+ 2 3 7)";       // working
+  // const char* input = "+";               // working
+  // const char* input = "-";               // working
+  // const char* input = "*";               // working
+  // const char* input = "4";               // working
 
   char* input = malloc(256 * sizeof(char));
   Environment* global_environment = init_environment();
@@ -20,8 +22,11 @@ int main() {
     printf("\n\nJVLISP >> "); fgets(input, 256, stdin);     // READ
     Token* tokens = tokenize(input);                        // READ
     SchemeObject* expr = parser(&tokens);                   // READ
-    SchemeObject* result = eval(expr, global_environment);  // EVAL
+    SchemeObject* result = eval(expr, &global_environment); // EVAL
     print_scheme_object(result);                            // PRINT
+    // free_tokens(tokens);
+    free_object(expr);
+    free_object(result);
   }                                                         // LOOP
 
   return 0;
