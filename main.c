55f47a8b7a56505d0432937b155a74644eeb2ecf
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2025-03-10 13:58:02 -0300

    Added some memory frees in main

--- main.c
+++ main.c
@@ -3,21 +3,25 @@
 #include "eval.h"
 
 // TODO: free tokens after parsing
-// TODO: free environment
-// TODO: free Scheme expressions
 // TODO: remove exits and replace with NULL pointers
 
 int main() {
   char* input = malloc(256 * sizeof(char));
   Environment* global_environment = init_environment();
 
-  while(1){                                                 // LOOP
-    printf("\n\nJVLISP >> "); fgets(input, 256, stdin);     // READ
-    Token* tokens = tokenize(input);                        // READ
-    SchemeObject* expr = parser(&tokens);                   // READ
-    SchemeObject* result = eval(expr, &global_environment); // EVAL
-    print_scheme_object(result);                            // PRINT
-  }                                                         // LOOP
+  while(1){
+    printf("\n\nJVLISP >> "); fgets(input, 256, stdin);
+    Token* tokens = tokenize(input);
+    SchemeObject* expr = parser(&tokens);
+    SchemeObject* result = eval(expr, &global_environment);
+    print_scheme_object(result);
 
+    free_scheme_object(expr);
+    free_scheme_object(result);
+  }
+
+  free_environment(global_environment);
+
+  printf("\n\nThanks for using JVLISP! :)\n");
   return 0;
 }
