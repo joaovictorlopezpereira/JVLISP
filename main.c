5efb09c3ba595c02e81bc0bb008f0d6aaf07f5b6
João Victor Lopez Pereira <142049069+joaovictorlopezpereira@users.noreply.github.com>
2025-03-09 22:06:54 -0300

    Added REPL

--- main.c
+++ main.c
@@ -2,35 +2,27 @@
 #include "eval.h"
 
 int main() {
-  const char* input = "(define x 10)"; // not working
-  // const char* input = "(/ 5 2 2)"; // working
-  // const char* input = "(* 1 3 7)"; // working
-  // const char* input = "(+ 5 (- 4 3) 1)"; //working
-  // const char* input = "(- 5 2 1)"; // working
-  // const char* input = "(+ 2 3 7)"; // working
-  // const char* input = "+"; // working
-  // const char* input = "-"; // working
-  // const char* input = "*"; // working!
-  // const char* input = "4";    // working!
+  // const char* input = "(define x 10)"; // not working
+  // // const char* input = "(/ 5 2 2)"; // working
+  // // const char* input = "(* 1 3 7)"; // working
+  // // const char* input = "(+ 5 (- 4 3) 1)"; //working
+  // // const char* input = "(- 5 2 1)"; // working
+  // // const char* input = "(+ 2 3 7)"; // working
+  // // const char* input = "+"; // working
+  // // const char* input = "-"; // working
+  // // const char* input = "*"; // working!
+  // // const char* input = "4";    // working!
 
+  char* input = malloc(256 * sizeof(char));
+  Environment* global_environment = init_environment();
 
-  printf("INPUT EXPRESSION:\n");
-  printf("%s", input);
+  while(1){                                                 // LOOP
+    printf("\n\nJVLISP >> "); fgets(input, 256, stdin);     // READ
+    Token* tokens = tokenize(input);                        // READ
+    SchemeObject* expr = parser(&tokens);                   // READ
+    SchemeObject* result = eval(expr, global_environment);  // EVAL
+    print_scheme_object(result);                            // PRINT
+  }                                                         // LOOP
 
-  printf("\n\nTOKENIZING:\n");
-  Token* tokens = tokenize(input);
-  for (int i = 0; tokens[i].type != TOKEN_EOF; i++) {
-    printf("Token Type %d: %s\n", tokens[i].type, tokens[i].value);
-  }
-
-  printf("\n\nPARSING:\n");
-  SchemeObject* expr = parser(&tokens);
-  print_scheme_object(expr); printf("\n");
-
-  printf("\n\nEVALUATING:\n");
-  SchemeObject* result = eval(expr, init_environment());
-  print_scheme_object(result);
-
-  printf("\n\nFinished executing.");
   return 0;
 }
